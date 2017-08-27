09ee73cb9f799d3f9f5b033cb92f46bde0d80813
ianmarx <i.marx.19@dartmouth.edu>
2017-08-27 17:19:03 -0400

    Modify addWorkout form layout

--- src/containers/forms/add-workout-form.js
+++ src/containers/forms/add-workout-form.js
@@ -78,7 +78,7 @@ class AddWorkoutForm extends Component {
       return (
         <div className="form-container">
           <form className="modal-form" onSubmit={this.onSubmit}>
-            <div className="form-title">Add Team Workout</div>
+            <div className="form-title">Add Workout</div>
             <div className="column-group">
               <ul className="form-column">
                 <li id="type-field">
@@ -137,50 +137,47 @@ class AddWorkoutForm extends Component {
               <li>
                 <h3>Activity</h3>
                 {this.state.activity === 'erg' &&
-                  <select required value={this.state.activity} onChange={this.onActivityChange}>
-                    <option default value="erg">Ergometer</option>
-                  </select>
+                  <div className="static-activity">Ergometer</div>
                 }
                 {this.state.activity === 'row' &&
-                  <select required value={this.state.activity} onChange={this.onActivityChange}>
-                    <option default value="row">Rowing</option>
-                  </select>
+                  <div className="static-activity">Rowing</div>
                 }
                 {this.state.activity === 'run' &&
-                  <select required value={this.state.activity} onChange={this.onActivityChange}>
-                    <option default value="run">Running</option>
-                  </select>
+                  <div className="static-activity">Running</div>
                 }
                 {this.state.activity === 'bike' &&
-                  <select required value={this.state.activity} onChange={this.onActivityChange}>
-                    <option default value="bike">Cycling</option>
-                  </select>
+                  <div className="static-activity">Cycling</div>
                 }
               </li>
               <li>
                 <h3>Distance Units</h3>
                 {this.state.activity === 'erg' &&
                   <select required value={this.state.distUnit} onChange={this.onDistUnitChange}>
-                    <option default value="m">m</option>
+                    <option default value="">Select</option>
+                    <option value="m">m</option>
+                    <option value="km">km</option>
                   </select>
                 }
                 {this.state.activity === 'row' &&
                   <select required value={this.state.distUnit} onChange={this.onDistUnitChange}>
-                    <option default value="km">km</option>
-                    <option default value="m">m</option>
+                    <option default value="">Select</option>
+                    <option value="km">km</option>
+                    <option value="m">m</option>
                   </select>
                 }
                 {this.state.activity === 'run' &&
                   <select required value={this.state.distUnit} onChange={this.onDistUnitChange}>
-                    <option default value="mi">mi</option>
-                    <option default value="km">km</option>
-                    <option default value="m">m</option>
+                    <option default value="">Select</option>
+                    <option value="mi">mi</option>
+                    <option value="km">km</option>
+                    <option value="m">m</option>
                   </select>
                 }
                 {this.state.activity === 'bike' &&
                   <select required value={this.state.distUnit} onChange={this.onDistUnitChange}>
-                    <option default value="mi">mi</option>
-                    <option default value="km">km</option>
+                    <option default value="">Select</option>
+                    <option value="mi">mi</option>
+                    <option value="km">km</option>
                   </select>
                 }
               </li>
@@ -192,7 +189,7 @@ class AddWorkoutForm extends Component {
                   />
                 </li>
               }
-              {(this.state.activity === 'erg' || this.state.activity === 'bike') &&
+              {(this.state.activity === 'bike') &&
                 <li>
                   <h3>Watts</h3>
                   <input onChange={this.onWattsChange} value={this.state.watts}
