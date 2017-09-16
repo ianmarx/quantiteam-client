701c2aba6a3d3a48cd21f33e9b66937273098448
ianmarx <i.marx.19@dartmouth.edu>
2017-09-15 20:46:39 -0400

    Display chart with erg/row totals

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack } from 'victory';
 import { fetchUser, fetchUserWorkouts, updateWorkout,
-  deleteWorkout, addTeam, fetchUserTeam } from '../actions';
+  deleteWorkout, addTeam, fetchUserTeam, fetchUserDistTotals } from '../actions';
 import WorkoutPost from './workout-post';
 
 const mapStateToProps = state => (
@@ -11,14 +12,24 @@ const mapStateToProps = state => (
     workouts: state.workouts.list,
     team: state.team.team,
     authenticated: state.auth.authenticated,
+    userDistTotal: state.workouts.userDistTotal,
   }
 );
 
 class Profile extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      ergTotals: [
+        { week: 1, distance: (this.props.user.ergTotal / 1000) || 0 },
+      ],
+      rowTotals: [
+        { week: 1, distance: (this.props.user.rowTotal / 1000) || 0 },
+      ],
+    };
     this.displayFeed = this.displayFeed.bind(this);
     this.displayInfo = this.displayInfo.bind(this);
+    this.displayWeekChart = this.displayWeekChart.bind(this);
     this.onDeleteClick = this.onDeleteClick.bind(this);
   }
   componentDidMount() {
@@ -29,6 +40,7 @@ class Profile extends Component {
     this.props.fetchUser(this.props.match.params.userId);
     this.props.fetchUserWorkouts(this.props.match.params.userId);
     this.props.fetchUserTeam(this.props.match.params.userId);
+//    this.props.fetchUserDistTotals(this.props.match.params.userId);
   }
   onDeleteClick(workoutId, userId) {
     this.props.deleteWorkout(workoutId, userId);
@@ -67,11 +79,46 @@ class Profile extends Component {
       </div>
     );
   }
+  displayWeekChart() {
+    if ((this.props.user.ergTotal === 0) || (this.props.user.rowTotal === 0)) {
+      return <div />;
+    } else {
+      return (
+        <VictoryChart
+          domainPadding={20}
+        >
+          <VictoryAxis
+            tickValues={[1]}
+            tickFormat={['Workout Totals']}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={x => (`${x}km`)}
+          />
+          <VictoryStack>
+            <VictoryBar
+              data={this.state.ergTotals}
+              style={{ data: { fill: '#eda412' } }}
+              x="week"
+              y="distance"
+            />
+            <VictoryBar
+              data={this.state.rowTotals}
+              style={{ data: { fill: '#2b85bc' } }}
+              x="week"
+              y="distance"
+            />
+          </VictoryStack>
+        </VictoryChart>
+      );
+    }
+  }
   render() {
     return (
       <div className="profile-page">
         <div className="profile-column">
           {this.displayInfo()}
+          {this.displayWeekChart()}
         </div>
         <div className="profile-column">
           <div id="feed-title">My Workouts</div>
@@ -83,4 +130,4 @@ class Profile extends Component {
 }
 
 export default withRouter(connect(mapStateToProps, { fetchUser, fetchUserWorkouts,
-  updateWorkout, deleteWorkout, addTeam, fetchUserTeam })(Profile));
+  updateWorkout, deleteWorkout, addTeam, fetchUserTeam, fetchUserDistTotals })(Profile));
