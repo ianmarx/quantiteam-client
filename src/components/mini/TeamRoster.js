5687b2dc0247cf8aecfaa203d82fd32e119a0e8a
ianmarx <ianpmarx@gmail.com>
2018-08-26 12:40:27 -0400

    Styling Updates
    Improve spacing in roster component
    
    Increase button width for team workout activity form

--- src/components/mini/TeamRoster.js
+++ src/components/mini/TeamRoster.js
@@ -5,41 +5,44 @@ const TeamRoster = (props) => {
   return (
     <div className='team-roster-modal-container'>
       <div className='team-roster'>
-        <div className='list-title'>Athletes</div>
-        {props.athletes.length === 0 ? (
-          <div>No athletes found.</div>
-        ) : (
-          <div>
-            {props.athletes.map((athlete, i) => {
-              return (
-                <div key={`athlete-${i}`}>
-                  <NavLink to={`/profile/${athlete._id}`}>
-                    <div className="profile-link">{athlete.name}</div>
-                  </NavLink>
-                </div>
-              );
-            })}
-          </div>
-        )}
-        <br />
-        <div className='list-title'>Coaches</div>
-        {props.coaches.length === 0 ? (
-          <div>No coaches found.</div>
-        ) : (
-          <div>
-            {props.coaches.map((coach, i) => {
-              return (
-                <div key={`coach-${i}`}>
-                  <NavLink to={`/profile/${coach._id}`}>
-                    <div className="profile-link">{coach.name}</div>
-                  </NavLink>
-                </div>
-              );
-            })}
-          </div>
-        )}
-        <br />
-        <button type='button' className='modal-close' onClick={props.onCloseRosterClick}>Close</button>
+        <div className='athlete-list'>
+          <div className='list-title'>Athletes</div>
+          {props.athletes.length === 0 ? (
+            <div>No athletes found.</div>
+          ) : (
+            <div>
+              {props.athletes.map((athlete, i) => {
+                return (
+                  <div key={`athlete-${i}`}>
+                    <NavLink to={`/profile/${athlete._id}`}>
+                      <div className="profile-link">{athlete.name}</div>
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        <div className='coach-list'>
+          <div className='list-title'>Coaches</div>
+          {props.coaches.length === 0 ? (
+            <div>No coaches found.</div>
+          ) : (
+            <div>
+              {props.coaches.map((coach, i) => {
+                return (
+                  <div key={`coach-${i}`}>
+                    <NavLink to={`/profile/${coach._id}`}>
+                      <div className="profile-link">{coach.name}</div>
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+          <br />
+          <button type='button' className='modal-close' onClick={props.onCloseRosterClick}>Close</button>
+        </div>
       </div>
     </div>
   );
