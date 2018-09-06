cd302ce0605912c0233b3f50ad7914a1da501d5d
ianmarx <ianpmarx@gmail.com>
2018-09-06 01:21:06 -0400

    Refactor render content into separate components, remove unused methods

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { signUpAthlete, signUpCoach } from '../actions/auth';
 import { checkTeamName, checkTeamCode } from '../actions/team';
+import SignUpForm from '../components/forms/SignUpForm';
+import UserTypeForm from '../components/forms/UserTypeForm';
 
 export const mapStateToProps = state => (
   {
@@ -40,10 +42,6 @@ export class SignUp extends Component {
     this.onBackClick = this.onBackClick.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.shouldDisableNextButton = this.shouldDisableNextButton.bind(this);
-    this.shouldShowTeamCode = this.shouldShowTeamCode.bind(this);
-    this.shouldShowTeamName = this.shouldShowTeamName.bind(this);
-    this.shouldShowAvailableIcon = this.shouldShowAvailableIcon.bind(this);
-    this.shouldShowValidIcon = this.shouldShowValidIcon.bind(this);
   }
 
   componentDidMount() {
@@ -125,148 +123,43 @@ export class SignUp extends Component {
     }
   }
 
-  shouldShowTeamCode() {
-    return this.state.userType === 'athlete' && !this.state.firstStepComplete;
-  }
-
-  shouldShowTeamName() {
-    return this.state.userType === 'coach' && !this.state.firstStepComplete;
-  }
-
-  shouldShowAvailableIcon() {
-    return this.state.teamName !== '' && this.props.teamNameIsAvailable;
-  }
-
-  shouldShowValidIcon() {
-    return this.state.teamCode !== '' && this.props.teamCodeIsValid;
-  }
-
-
   render() {
     return (
-      <div className="signup-form">
-        <form onSubmit={this.onSubmit}>
-          <h2>Sign Up</h2>
-          {!this.state.firstStepComplete &&
-            <div className='button-group'>
-              <button
-                className={`athlete ${this.state.userType !== 'athlete' && 'disabled'}`}
-                type='button'
-                onClick={this.onAthleteSelect}
-              >
-                Athlete
-              </button>
-              <button
-                className={`coach ${this.state.userType !== 'coach' && 'disabled'}`}
-                type='button'
-                onClick={this.onCoachSelect}
-              >
-                Coach
-              </button>
-            </div>
-          }
-          {this.shouldShowTeamCode() &&
-            <div className="team-code field">
-              <div>Enter the team code provided by your coach.</div>
-              <input
-                className='team-code-input'
-                onChange={this.onTeamCodeChange}
-                value={this.state.teamCode}
-                type="text"
-                required
-                placeholder='Team Code'
-                autoFocus
-                spellCheck={false}
-              />
-              {this.shouldShowValidIcon() &&
-                <span className='fa-stack'>
-                  <i className='fas fa-circle fa-stack-1x' />
-                  <i className='fas fa-check-circle fa-stack-1x' />
-                </span>
-              }
-            </div>
-          }
-          {this.shouldShowTeamName() &&
-            <div className="team-name field">
-              <div>Enter your desired team name to check availability.</div>
-              <input
-                className='team-name-input'
-                onChange={this.onTeamNameChange}
-                value={this.state.teamName}
-                type="text"
-                required
-                placeholder='Team Name'
-                autoFocus
-                spellCheck={false}
-              />
-              {this.shouldShowAvailableIcon() &&
-                <span className='fa-stack'>
-                  <i className='fas fa-circle fa-stack-1x' />
-                  <i className='fas fa-check-circle fa-stack-1x' />
-                </span>
-              }
-            </div>
-          }
-          {this.state.firstStepComplete ? (
-            <div>
-              <div className="field">
-                <input
-                  className='name'
-                  onChange={this.onNameChange}
-                  value={this.state.name}
-                  type="text"
-                  required
-                  placeholder='Name'
-                  autoComplete='name'
-                />
-              </div>
-              <div className="field">
-                <input
-                  className='email'
-                  onChange={this.onEmailChange}
-                  value={this.state.email}
-                  type="text"
-                  required
-                  placeholder='Email'
-                  autoComplete='email'
-                />
-              </div>
-              <div className="field">
-                <input
-                  className='password'
-                  onChange={this.onPasswordChange}
-                  value={this.state.password}
-                  type="password"
-                  required
-                  placeholder='Password'
-                  autoComplete='new-password'
-                />
-              </div>
-              <div className='field'>
-                <button type="submit" className="signup-button">Sign Up</button>
-                <br />
-                <button className='back-button' onClick={this.onBackClick}>
-                  Back
-                </button>
-              </div>
-              {this.props.statusText &&
-                <div className='status-text'>
-                  {this.props.statusText}
-                </div>
-              }
-            </div>
-          ) : (
-            <div>
-              <button className='next-button' disabled={this.shouldDisableNextButton()} onClick={this.onNextClick}>
-                Next
-              </button>
-              <br />
-              <NavLink to="/">
-                <button className="back-button">Back</button>
-              </NavLink>
-            </div>
-          )}
-        </form>
+      <div className='sign-up-container'>
+        {this.state.firstStepComplete ? (
+          <SignUpForm
+            email={this.state.email}
+            name={this.state.name}
+            onBackClick={this.onBackClick}
+            onEmailChange={this.onEmailChange}
+            onNameChange={this.onNameChange}
+            onPasswordChange={this.onPasswordChange}
+            onSubmit={this.onSubmit}
+            password={this.state.password}
+            statusText={this.props.statusText}
+          />
+        ) : (
+          <UserTypeForm
+            onAthleteSelect={this.onAthleteSelect}
+            onCoachSelect={this.onCoachSelect}
+            onNextClick={this.onNextClick}
+            onTeamCodeChange={this.onTeamCodeChange}
+            onTeamNameChange={this.onTeamNameChange}
+            disableNextButton={this.shouldDisableNextButton()}
+            teamCode={this.state.teamCode}
+            teamCodeIsValid={this.props.teamCodeIsValid}
+            teamName={this.state.teamName}
+            teamNameIsAvailable={this.props.teamNameIsAvailable}
+            userType={this.state.userType}
+          />
+        )
+        }
+        <div className='line' />
+        <div className='info-box'>
+          <div className='col even'>
+            <div className='p'>&copy; 2018 QuantiTeam. All rights reserved.</div>
+          </div>
+        </div>
       </div>
     );
   }
